81bd8d9c2361b2f7bdde1d8a9b0618b81c9b108e
Joseph <yusefhalabi456@gmail.com>
2023-05-04 18:08:10 +0300

    fetch weather data from weather api function

---
+++ app.js
@@ -0,0 +1,27 @@
+
+
+async function getWeather(location){
+    try{
+        const response = await fetch(
+            `http://api.weatherapi.com/v1/forecast.json?key=b39abc9b9360493b82290412232804&q=${location}`,
+            {
+            mode: 'cors',
+            }
+        );
+    
+        const weatherData = await response.json();
+        displayData(weatherData);
+    }
+    catch(e){
+        alert(e);
+    }
+}
+
+function displayData(data){
+    console.log(data);
+    console.log(data.current.temp_c);
+
+}
+
+
+getWeather('london');
