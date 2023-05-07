b77a16358b3f45bad40a4f8591de39eb169fc0d6
Joseph <yusefhalabi456@gmail.com>
2023-05-07 20:19:14 +0300

    finish display functions

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-
+const weatherCard = document.querySelector('.weather-card');
+const country = document.getElementById('country');
+const cityName = document.getElementById('city');
+const temp = document.getElementById('temp');
+const feelsLike = document.getElementById('feels-like');
+const humidity = document.getElementById('humidity');
+const wind = document.getElementById('wind');
 
 async function getWeather(location){
     try{
@@ -18,10 +24,37 @@ async function getWeather(location){
 }
 
 function displayData(data){
+    initializeDataInfo(data);
+    appendInfoToContainer();
+    
+    
+    
     console.log(data);
     console.log(data.current.temp_c);
 
 }
 
+function initializeDataInfo(data){
+    country.innerHTML = data.location.country;
+    cityName.innerHTML = data.location.name;
+    temp.innerHTML = data.current.temp_c + " °C";
+    feelsLike.innerHTML = "Feels like: " + data.current.feelslike_c + " °C";
+    humidity.innerHTML = "Humidity: " + data.current.humidity + " %"
+    wind.innerHTML = "Wind Speed: " + data.current.wind_kph + " km/h"
 
-getWeather('Isfiya');
+
+}
+
+function appendInfoToContainer(){
+    weatherCard.appendChild(country);
+    weatherCard.appendChild(cityName);
+    weatherCard.appendChild(temp);
+    weatherCard.appendChild(feelsLike);
+    weatherCard.appendChild(humidity);
+    weatherCard.appendChild(wind);
+}
+
+
+
+
+getWeather('Haifa');
